0672e9dd101b8d657d3958bb01f059bf3330f8ef
andre010101 <andrepessoa182@hotmail.com>
2022-12-10 12:26:22 -0300

    atualização projeto

--- Projeto_Final_Atos/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Projeto_Final_Atos.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

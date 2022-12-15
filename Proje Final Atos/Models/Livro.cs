3943981d8e3843838e2cec9bf8291370620ddfde
andre010101 <andrepessoa182@hotmail.com>
2022-12-14 22:55:25 -0300

    Personagens

--- Proje Final Atos/Models/Livro.cs
+++ Proje Final Atos/Models/Livro.cs
@@ -1,17 +1,23 @@
-﻿using Microsoft.Build.Framework;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Proje_Final_Atos.Models
 {
     public class Livro
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "O nome do usuário é obrigatório", AllowEmptyStrings = false)]
+        [Required(ErrorMessage = "Informe o Titulo do Livro")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "Informe o SubTitulo do Livro")]
         public string SubTitulo { get; set; }
+        [Required(ErrorMessage = "Informe o Autor do Livro")]
         public string Autor { get; set; }
+        [Required(ErrorMessage = "Informe o Genero do Livro")]
         public string Genero { get; set; }
+        [Required(ErrorMessage = "Informe o Idioma do Livro")]
         public string Idioma { get; set; }
+        [Required(ErrorMessage = "Informe a Capa do Livro")]
         public string Capa { get; set; }
+        [Required(ErrorMessage = "Informe a Resenha do Livro")]
         public string Resenha { get; set; }
     }
 }

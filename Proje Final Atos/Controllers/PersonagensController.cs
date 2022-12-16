afdcb0c9faca96a12c5308de4af1abea13fb5399
andre010101 <andrepessoa182@hotmail.com>
2022-12-15 21:21:42 -0300

    Criação da Page Hermione

---
+++ Proje Final Atos/Controllers/PersonagensController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proje_Final_Atos.Controllers
+{
+    public class PersonagensController : Controller
+    {
+        public IActionResult Hermione()
+        {
+            return View();
+        }
+    }
+}
